9ee0123ed69a659985b1d4a70690cdcbd9e312f5
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-06-04 10:21:42 +0900

    Update 경로를 넣는 거 dcmtonii.py

--- 수동 변환/경로를 넣는 거 dcmtonii.py
+++ 수동 변환/경로를 넣는 거 dcmtonii.py
@@ -18,17 +18,17 @@ def convert_dicom_to_nifti_interactive():
         print("[X] 경로가 존재하지 않음:", dicom_folder)
         return
 
-    ## TotalSegmentator에서 이루어진 슬라이스 간격 검증 끄기
+    ##TotalSegmentator에서 이루어진 슬라이스 간격 검증 끄기
     dicom2nifti.settings.disable_validate_slice_increment()
 
-    ## 처리 편의성을 위해 출력 파일 이름 구성
+    ##처리 편의성을 위해 출력 파일 이름 구성
     patient_id = extract_patient_id(dicom_folder)
     phase = os.path.basename(dicom_folder).upper()  # PRE, POST 등
     output_folder = os.path.join(os.getcwd(), "converted_output")
     os.makedirs(output_folder, exist_ok=True)
     output_nii_path = os.path.join(output_folder, f"{patient_id}_{phase}.nii.gz")
 
-    ## 디버깅할 DICOM 읽기
+    ##디버깅할 DICOM 읽기
     dcm_files = []
     for fname in os.listdir(dicom_folder):
         if fname.lower().endswith(".dcm") or '.' not in fname:
